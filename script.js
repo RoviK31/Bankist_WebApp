e2cdbf4abdf4d128f509c1505277231af9040bb7
RoviK31 <rovikeith.navarro.y@bulsu.edu.ph>
2023-09-22 16:37:36 +0800

    lecture: javascript events

--- script.js
+++ script.js
@@ -103,3 +103,23 @@ btnScrollTo.addEventListener('click', function (e) {
 //modern way
 section1.scrollIntoView({behavior: 'smooth'})
 });
+
+
+const h1 = document.querySelector('h1')
+
+// h1.addEventListener('mouseenter', function(e){
+//   alert('addEventListener: Great! You are reading the heading :D')
+// })
+const alertH1 = function(e){
+  alert('addEventListener : Great! You are reading the heading :D')
+}
+
+h1.addEventListener('mouseenter', alertH1)
+setTimeout(()=>
+h1.removeEventListener('mouseenter', alertH1),3000)
+
+
+
+h1.onmouseenter = function(e){
+  alert('onmouseenter: Great! You are reading the heading :D')
+}
