c0223df57459734030b71b53c421d9c10cc50564
RoviK31 <rovikeith.navarro.y@bulsu.edu.ph>
2023-09-22 16:55:22 +0800

    lecture: bubling and capturing

--- script.js
+++ script.js
@@ -105,21 +105,38 @@ section1.scrollIntoView({behavior: 'smooth'})
 });
 
 
-const h1 = document.querySelector('h1')
+// const h1 = document.querySelector('h1')
 
-// h1.addEventListener('mouseenter', function(e){
-//   alert('addEventListener: Great! You are reading the heading :D')
+// // h1.addEventListener('mouseenter', function(e){
+// //   alert('addEventListener: Great! You are reading the heading :D')
+// // })
+// const alertH1 = function(e){
+//   alert('addEventListener : Great! You are reading the heading :D')
+// }
+
+// h1.addEventListener('mouseenter', alertH1)
+// setTimeout(()=>
+// h1.removeEventListener('mouseenter', alertH1),3000)
+
+
+
+// h1.onmouseenter = function(e){
+//   alert('onmouseenter: Great! You are reading the heading :D')
+// }
+
+// const randomInt = function(min, max){
+//   Math.floor(Math.random() * (max - min + 1) + min)
+// }
+
+// const randomColor= () => `rgb(${randomInt(0,255)},rgb(${randomInt(0,255)},rgb(${randomInt(0,255)})`
+
+// document.querySelector('.nav__link').addEventListener('click', function(e){
+//   this.style.backgroundColor = randomColor()
 // })
-const alertH1 = function(e){
-  alert('addEventListener : Great! You are reading the heading :D')
-}
+// document.querySelector('.nav__links').addEventListener('click', function(e){
+//   this.style.backgroundColor = randomColor()
+// })
 
-h1.addEventListener('mouseenter', alertH1)
-setTimeout(()=>
-h1.removeEventListener('mouseenter', alertH1),3000)
-
-
-
-h1.onmouseenter = function(e){
-  alert('onmouseenter: Great! You are reading the heading :D')
-}
+// document.querySelector('.nav').addEventListener('click', function(e){
+//   this.style.backgroundColor = randomColor()
+// })
