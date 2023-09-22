4d00448f6381d54959f90b0cf013648c5acf4e4c
RoviK31 <rovikeith.navarro.y@bulsu.edu.ph>
2023-09-22 15:58:40 +0800

    add cookie message

--- script.js
+++ script.js
@@ -30,3 +30,18 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+////////////////////////////////////////
+////////////////////////////////////////
+////////////////////////////////////////
+
+//Creating and insering elements
+const header = document.querySelector('.header')
+const message = document.createElement('div')
+message.classList.add('cookie-message')
+// message.textContent = 'We use cookie for improved functionality and analytics.'
+message.innerHTML = 'We use cookie for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>'
+header.append(message)
+
+document.querySelector('.btn--close-cookie').addEventListener('click', function(){
+  message.remove()
+})
