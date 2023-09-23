52dcb2345f1f27832a35730df8fa4cd75a53cb60
RoviK31 <rovikeith.navarro.y@bulsu.edu.ph>
2023-09-23 21:48:32 +0800

    passing argument into handler using bind

--- script.js
+++ script.js
@@ -129,31 +129,33 @@ tabsContainer.addEventListener('click', function (e) {
 });
 
 // Menu fade animation
-const nav = document.querySelector('.nav')
+const nav = document.querySelector('.nav');
 
-
-const handleHover = function(e, opacity){
-  if(e.target.classList.contains('nav__link')){
-    const link = e.target
-    const siblings = link.closest('.nav').querySelectorAll('.nav__link')
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
 
     siblings.forEach(el => {
-      if(el !== link){
-      el.style.opacity = opacity
-      } 
-    })
-    logo.style.opacity = opacity
+      if (el !== link) {
+        el.style.opacity = this
+      }
+    });
+    logo.style.opacity = this
   }
-}
+};
 
-nav.addEventListener('mouseover', function(e){
-  handleHover(e,0.5)
-})
+//padding an argument into handler using bind
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
 
-nav.addEventListener('mouseout', function(e){
-  handleHover(e,1)
-})
+// nav.addEventListener('mouseover', function(e){
+//   handleHover(e,0.5)
+// })
+// nav.addEventListener('mouseout', function(e){
+//   handleHover(e,1)
+// })
 
 // const h1 = document.querySelector('h1')
 
