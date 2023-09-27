7659553adc7916c2c02c8501d0ef5ae8ac915600
RoviK31 <rovikeith.navarro.y@bulsu.edu.ph>
2023-09-27 19:07:05 +0800

    add lecture about intersectionObserver API

--- script.js
+++ script.js
@@ -158,12 +158,28 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 // })
 
 //Sticky navigation
-const initialCoords = section1.getBoundingClientRect()  
-window.addEventListener('scroll', function(){
+// const initialCoords = section1.getBoundingClientRect()  
+// window.addEventListener('scroll', function(){
   
-  if(window.scrollY > initialCoords.top) nav.classList.add('sticky')
-  else nav.classList.remove('sticky')
-})
+//   if(window.scrollY > initialCoords.top) nav.classList.add('sticky')
+//   else nav.classList.remove('sticky')
+// })
+
+//Sticky navigation: Intersection Observer API
+const obsCallback = function(entries, observer){
+  entries.forEach(entry =>{
+    
+  })
+};
+const obsOptions = {
+  root: null,
+  threshold: [0, 0.2]
+}
+
+//intersectionObserver API needs two arguments call back function and object
+const observer = new IntersectionObserver(obsCallback,obsOptions);
+observer.observe(section1)
+
 
 
 // const h1 = document.querySelector('h1')
